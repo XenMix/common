667a316a4b460fb51943402438da4c3aa30e4ffa
XenMix <xenmix@live.fr>
2015-01-13 04:14:27 +0100

    4

--- faceit.js
+++ faceit.js
@@ -2,23 +2,23 @@ var plys = document.getElementsByClassName('player-content');
 var us = ["XenMix", "DE4THROW", "ZAKtw"];
 var side = 0;
 
-if (typeof jQuery != 'undefined') {
- 
+if (typeof jQuery != 'undefined'){
     console.log("jQuery library is loaded!");
- 
-}else{
- 
-    console.log("jQuery library is not found!");
- 
-function LevelIframe(id, name)
+} else
 {
-	$(id).find(".clearfix").append('<div style="level_ifrm_base"><iframe scrolling="no" src="https://www.faceit.com/players/'+name+'/csgo" class="level_ifrm"></iframe>');
+    console.log("jQuery library is not found!");
 }
 
 if (document.location.href.toString().indexOf("room") != -1)
 {
 	for (var i = 0 ; i < 5 ; i++) {
-		var str = $(plys[i]).find(".nickname").html();
+		var str = jQuery(plys[i]).find(".nickname").html();
+        console.log('ok' + str);
+		if (str === undefined)
+		{
+			break;
+		}
+		console.log('oki');
 		str = str.split("</a>");
 		str = str[0].split(">");
 		if( jQuery.inArray(str[1], us) != -1 )
@@ -29,8 +29,12 @@ if (document.location.href.toString().indexOf("room") != -1)
 	}
 	for (var i = side ; i < 5 + side ; i++) {
 		var str = $(plys[i]).find(".nickname").html();
+        if (str === undefined)
+		{
+			break;
+		}
 		str = str.split("</a>");
 		str = str[0].split(">");
-		LevelIframe(plys[i],str[1]);
+		$(plys[i]).find(".clearfix").append('<div style="level_ifrm_base"><iframe scrolling="no" src="https://www.faceit.com/players/'+str[1]+'/csgo" class="level_ifrm"></iframe>');
 	}
 }
