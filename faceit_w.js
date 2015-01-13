fe75d4f1f44b13eb2ce7ae6d6b2eced7ddbc51fa
XenMix <xenmix@live.fr>
2015-01-13 05:15:21 +0100

    22

--- faceit_w.js
+++ faceit_w.js
@@ -1,12 +1,10 @@
 setTimeout(function() {
-
-	var us = ["XenMix", "DE4THROW", "ZAKtw"];
-	var side = 0;
 	
 	console.log('Loaded');
 
 	if (document.location.href.toString().indexOf("room") != -1)
 	{
+		var us = ["XenMix", "DE4THROW", "ZAKtw"];
 		for (var i = 0 ; i < 5 ; i++) {
 			var plys = document.getElementsByClassName('player-content');
 			console.log('ok('+i+'): ' + plys[i]);
@@ -26,6 +24,7 @@ setTimeout(function() {
 			str = str.split("pushstate");
 			str = str[0].split(">");
 			str = str[1].split("<")
+			console.log(side);
 			$(plys[i]).find(".clearfix").append('<div style="position: relative;width: 50px;height: 50px"><iframe scrolling="no" src="https://www.faceit.com/players/'+str[1]+'/csgo" class="level_ifrm"></iframe>');
 		}
 	}
