64c227a176e6cc3c5504e9d1d8680dd35c7d7c7c
XenMix <xenmix@live.fr>
2015-01-13 03:00:56 +0100

    first
    test

---
+++ faceit.js
@@ -0,0 +1,28 @@
+var plys = document.getElementsByClassName('player-content');
+var us = ["XenMix", "DE4THROW", "ZAKtw"];
+var side = 0;
+
+function LevelIframe(id, name)
+{
+	$(id).find(".clearfix").append('<div style="level_ifrm_base"><iframe scrolling="no" src="https://www.faceit.com/players/'+name+'/csgo" class="level_ifrm"></iframe>');
+}
+
+if (document.location.href.toString().indexOf("room") != -1)
+{
+	for (var i = 0 ; i < 5 ; i++) {
+		var str = $(plys[i]).find(".nickname").html();
+		str = str.split("</a>");
+		str = str[0].split(">");
+		if( jQuery.inArray(str[1], us) != -1 )
+		{
+			side = 5;
+			break;
+		}
+	}
+	for (var i = side ; i < 5 + side ; i++) {
+		var str = $(plys[i]).find(".nickname").html();
+		str = str.split("</a>");
+		str = str[0].split(">");
+		LevelIframe(plys[i],str[1]);
+	}
+}
