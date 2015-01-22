90733c286ca4a20af259c7c5f8bd94bb6aaf95a8
XenMix <xenmix@live.fr>
2015-01-22 05:35:51 +0100

    555
    5555

--- faceit_oklm.js
+++ faceit_oklm.js
@@ -27,8 +27,10 @@ function OnPageSwitch()
 		for (var i = side ; i < 5 + side ; i++) {
 			var plys = document.getElementsByClassName('player-content');
 			var str = $(plys[i]).data('nickname');
-			var offleft = parseInt($(plys[i]).offset().left) + 10; 
-			$('body').append('<div style="z-index: 999;overflow: hidden; position: absolute; width:50px; height:50px; left:'+offleft+'px; top:'+ $(plys[i]).offset().top +'px"><iframe style="position: relative;left: -595px;bottom:405px;height: 550px;" width="700px" height="512px" scrolling="no" src="https://www.faceit.com/players/'+str+'/csgo" id="seek_iframe_'+i+'" class="level_ifrm"></iframe>');
+			var offleft = parseInt($(plys[i]).offset().left) + 10;
+			$.get('https://api.faceit.com/api/nicknames/'+str, function(data) {
+				$('body').append('<div id="seek_iframe_'+i+' class="level_ifrm" style="z-index: 999;overflow: hidden; text-size: 20px; position: absolute; width:50px; height:50px; left:'+offleft+'px; top:'+ $(plys[i]).offset().top +'px">'+data.payload.csgo_skill_level_label+'</div>');
+			});
 		}
 	}
 }
